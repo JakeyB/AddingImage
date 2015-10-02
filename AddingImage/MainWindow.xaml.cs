b970a3ebe461295a730cb5d3c7d9f922739e247a
jakey-b <jacobsteadman@hotmail.co.uk>
2015-10-02 14:56:23 +0100

    Initial code for login project 02/10/2015 14:56

---
+++ AddingImage/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Sql;
+
+using  System.Data.SQLite;
+using System.Windows.Markup;
+using System.Windows.Media.Animation;
+
+namespace AddingImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Define connection to database
+        string dbConnectionString = @"Data Source=database.db;Version=3;";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteConnection sqliteCon = new SQLiteConnection(dbConnectionString);
+            // Open connection to database
+            try
+            {
+                sqliteCon.Open();
+
+                string Query = "select * from employeeInfo where username='" + this.txt_username.Text + "' and password='" + this.txt_password.Password + "' ";
+                SQLiteCommand createCommand = new SQLiteCommand(Query, sqliteCon);
+
+                createCommand.ExecuteNonQuery();
+                SQLiteDataReader dr = createCommand.ExecuteReader();
+
+                int count = 0;
+
+                while (dr.Read())
+                {
+                    count++;
+                }
+
+                // If Passwords match open new window *** Will need to close the old window ***
+                if(count == 1){
+                    MessageBox.Show("Username and password are correct!");
+
+                    //Hide login page!
+                    this.Hide();
+
+                    // Close connection to database
+                    sqliteCon.Close();
+
+                    // Create an object of the new WPF window
+                    Window1 sec = new Window1();
+                    sec.ShowDialog();
+                } 
+                if (count > 1)
+                {
+                    MessageBox.Show("Duplicate Username and password, try again");  
+                }
+                if (count < 1)
+                {
+                    MessageBox.Show("Username or password incorrect!");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Image1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image img = (Image) sender;
+            DoubleAnimation animation = new DoubleAnimation(0,TimeSpan.FromSeconds(2));
+            img.BeginAnimation(Image.OpacityProperty, animation);
+        }
+
+        private void Image1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            DoubleAnimation animation = new DoubleAnimation(1, TimeSpan.FromSeconds(2));
+            img.BeginAnimation(Image.OpacityProperty, animation);
+        }
+    }
+}
---
+++ AddingImage/Window1.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+namespace AddingImage
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        //Define db connection
+        private string dbConnectionString = @"Data Source=database.db;Version=3;";
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_btn_Click(object sender, RoutedEventArgs e)
+        {
+            //Create new db connection
+            SQLiteConnection sqliteCon = new SQLiteConnection(dbConnectionString);
+
+            try
+            {
+                // Open connection to database
+                sqliteCon.Open();
+
+                string Query = "insert into employeeInfo (id,name,surname,age) values('" + this.ID_text.Text + "','" +
+                               this.Name_text.Text + "','" + this.Surname_text.Text + "','" + this.Age_text.Text + "')";
+                SQLiteCommand createCommand = new SQLiteCommand(Query, sqliteCon);
+                createCommand.ExecuteNonQuery();
+
+                MessageBox.Show("Saved!");
+                /*SQLiteDataReader dr = createCommand.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    
+                }
+                */
+
+                // Close connection to database
+                sqliteCon.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Update_button_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteConnection sqliteCon = new SQLiteConnection(dbConnectionString);
+
+            try
+            {
+                // Open connection to database
+                sqliteCon.Open();
+
+                string Query = "update employeeInfo set id='" + this.ID_text.Text + "', name='" + this.Name_text.Text +
+                               "', surname= '" + this.Surname_text.Text + "', age= '" + this.Age_text.Text +
+                               "' where id='" + this.ID_text.Text + "'  ";
+                SQLiteCommand createCommand = new SQLiteCommand(Query, sqliteCon);
+                createCommand.ExecuteNonQuery();
+
+                MessageBox.Show("Updated");
+                
+                //Close connection
+                sqliteCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Delete_button_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteConnection sqliteCon = new SQLiteConnection(dbConnectionString);
+
+            try
+            {
+                // Open connection to database
+                sqliteCon.Open();
+
+                string Query = "delete from employeeInfo where id='" +this.ID_text.Text+"'";
+                SQLiteCommand createCommand = new SQLiteCommand(Query, sqliteCon);
+                createCommand.ExecuteNonQuery();
+
+                MessageBox.Show("Deleted");
+                
+                sqliteCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
